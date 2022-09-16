65c84c6cb2260680546a60b00991e27675ac965a
Isaac Fowler <Isaac.scott.fowler@gmail.com>
2022-09-16 15:38:33 -0400

    test.cpp to test linked list

---
+++ Linked List Template/test.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include "node.hpp"
+#include "list.hpp"
+
+using std::cout; using std::endl; using std::string;
+
+int main() {
+        Collection<int> intTest;
+        cout << "creating a collection of ints" << endl;
+        intTest.add(1);
+        intTest.add(1);
+        intTest.add(2);
+        intTest.add(2);
+        intTest.add(3);
+        intTest.add(4);
+        intTest.add(5);
+        intTest.add(2);
+        intTest.add(3);
+        intTest.print();
+        cout << "last node in intTest is: " << intTest.last() << endl;
+        intTest.remove(2);
+        cout << "- removed 2:" << endl;
+        intTest.print();
+        intTest.remove(1);
+        cout << "- removed 1:" << endl;
+        intTest.print();
+
+        Collection<string> strTest;
+        cout << "creating a collection of strings" << endl;
+        strTest.add("Hello");
+        strTest.add("world!");
+        strTest.print();
+        cout << "last node in strTest is: " << strTest.last() << endl;
+}
